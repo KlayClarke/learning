2bf7c3828f2953c3bf61edc255e5f20832ba1c57
Klay Clarke <klayaclarke@gmail.com>
2022-02-19 01:38:46 -0500

    add input field for client side api search

--- front-end/learning-js/api/script.js
+++ front-end/learning-js/api/script.js
@@ -1,16 +1,51 @@
 const img = document.querySelector("img");
+const randomizeButton = document.querySelector("button");
+const searchButton = document.querySelector("button[type='submit']");
 
-console.log(img);
+function fetchRandomGif() {
+  fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=KAEH8Nhd18a775RhjCjbuh1We6qSLMkV&s=beautiful",
+    {
+      mode: "cors",
+    }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    })
+    .catch((e) => {
+      img.src =
+        "https://images.unsplash.com/photo-1531686264889-56fdcabd163f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmlyZXdvcmt8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60";
+      console.log(e);
+    });
+}
 
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=KAEH8Nhd18a775RhjCjbuh1We6qSLMkV&s=beautiful",
-  {
-    mode: "cors",
-  }
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    img.src = response.data.images.original.url;
-  });
+function searchForGif() {
+  let val = document.querySelector("input").value;
+  fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=KAEH8Nhd18a775RhjCjbuh1We6qSLMkV&s=${val}`,
+    {
+      mode: "cors",
+    }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    })
+    .catch((e) => {
+      img.src =
+        "https://images.unsplash.com/photo-1531686264889-56fdcabd163f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmlyZXdvcmt8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60";
+      console.log(e);
+    });
+
+  document.querySelector("input").value = "";
+}
+
+fetchRandomGif();
+
+randomizeButton.addEventListener("click", fetchRandomGif);
+searchButton.addEventListener("click", searchForGif);
