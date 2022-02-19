2df50247e29657b4f9842faee040a9d14ea4eff8
Klay Clarke <klayaclarke@gmail.com>
2022-02-19 10:07:38 -0500

    clean up previous asynchronous code by using async functions and awaiting (as opposed to fetching, then and catching)

--- front-end/learning-js/api/script.js
+++ front-end/learning-js/api/script.js
@@ -2,47 +2,27 @@ const img = document.querySelector("img");
 const randomizeButton = document.querySelector("button");
 const searchButton = document.querySelector("button[type='submit']");
 
-function fetchRandomGif() {
-  fetch(
+async function fetchRandomGif() {
+  let response = await fetch(
     "https://api.giphy.com/v1/gifs/translate?api_key=KAEH8Nhd18a775RhjCjbuh1We6qSLMkV&s=beautiful",
     {
       mode: "cors",
     }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      img.src = response.data.images.original.url;
-    })
-    .catch((e) => {
-      img.src =
-        "https://images.unsplash.com/photo-1531686264889-56fdcabd163f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmlyZXdvcmt8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60";
-      console.log(e);
-    });
+  );
+  let json = await response.json();
+  img.src = json.data.images.original.url;
 }
 
-function searchForGif() {
+async function searchForGif() {
   let val = document.querySelector("input").value;
-  fetch(
+  let response = await fetch(
     `https://api.giphy.com/v1/gifs/translate?api_key=KAEH8Nhd18a775RhjCjbuh1We6qSLMkV&s=${val}`,
     {
       mode: "cors",
     }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      img.src = response.data.images.original.url;
-    })
-    .catch((e) => {
-      img.src =
-        "https://images.unsplash.com/photo-1531686264889-56fdcabd163f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmlyZXdvcmt8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60";
-      console.log(e);
-    });
-
-  document.querySelector("input").value = "";
+  );
+  let json = await response.json();
+  img.src = json.data.images.original.url;
 }
 
 fetchRandomGif();
