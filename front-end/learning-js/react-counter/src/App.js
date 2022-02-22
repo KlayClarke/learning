ace19289b4137b0c81d9b594c06b470303f3db4e
Klay Clarke <klayaclarke@gmail.com>
2022-02-21 21:29:57 -0500

    create counter (button that when clicked adds to curently displayed count)

---
+++ front-end/learning-js/react-counter/src/App.js
@@ -0,0 +1,31 @@
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      count: 0,
+    };
+
+    this.countUp = this.countUp.bind(this);
+  }
+
+  countUp() {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.countUp}>Click Me!</button>
+        <p>{this.state.count}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
