46bf5deaf7d0932dedd6886c516f464937a5ed6c
Klay Clarke <klayaclarke@gmail.com>
2022-02-21 21:05:48 -0500

    react app demo

---
+++ front-end/learning-js/react-app-demo/src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+
+import MyComponent from "./MyComponent";
+import { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onClickBtn = this.onClickBtn.bind(this);
+  }
+
+  onClickBtn() {
+    console.log("Button has been clicked");
+  }
+
+  render() {
+    return (
+      <div>
+        <MyComponent title="React" onButtonClicked={this.onClickBtn} />
+      </div>
+    );
+  }
+}
+
+export default App;
