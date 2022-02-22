ef429cc2047f7af9c3bfb2b072c32e9896d2ffba
Klay Clarke <klayaclarke@gmail.com>
2022-02-21 22:15:23 -0500

    create component and app using functional components (as opposed to the class components previously used)

--- front-end/learning-js/react-counter/src/App.js
+++ front-end/learning-js/react-counter/src/App.js
@@ -1,31 +1,40 @@
 import { Component } from "react";
 import "./App.css";
+import MyComponent from "./MyComponents";
 
-class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      count: 0,
-    };
-
-    this.countUp = this.countUp.bind(this);
-  }
-
-  countUp() {
-    this.setState({
-      count: this.state.count + 1,
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.countUp}>Click Me!</button>
-        <p>{this.state.count}</p>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <MyComponent title="Hello World" />
+    </div>
+  );
 }
+
+// class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       count: 0,
+//     };
+
+//     this.countUp = this.countUp.bind(this);
+//   }
+
+//   countUp() {
+//     this.setState({
+//       count: this.state.count + 1,
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.countUp}>Click Me!</button>
+//         <p>{this.state.count}</p>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
