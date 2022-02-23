541d886f2d482ef57b8e55554b94cbfd3e212718
Klay Clarke <klayaclarke@gmail.com>
2022-02-23 18:52:42 -0500

    fetch data from fortnite api and render data (upcoming store items) to page in shop route of web app

--- front-end/learning-js/react-router-demo-v2/src/Shop.js
+++ front-end/learning-js/react-router-demo-v2/src/Shop.js
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 const Shop = () => {
+  const [items, setItems] = useState([]);
+
+  const fetchItems = async () => {
+    const response = await fetch(
+      "https://fortnite-api.theapinetwork.com/upcoming/get"
+    );
+    let data = await response.json();
+    setItems(data.data);
+  };
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  let itemKey = 0;
+
   return (
     <div>
       <main>
-        <h1>Main Content On Shop Page Goes Here</h1>
+        {items.map((item) => (
+          <h1 key={item.itemId}>{item.item.name}</h1>
+        ))}
       </main>
     </div>
   );
