3026719be328faa7509040644d0a7637c22f4799
Klay Clarke <klayaclarke@gmail.com>
2022-02-22 21:56:39 -0500

    react hooks using useEffect

--- front-end/learning-js/react-hooks/src/App.js
+++ front-end/learning-js/react-hooks/src/App.js
@@ -1,18 +1,40 @@
-import { Component, useState } from "react";
+import { Component, useState, useEffect } from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("black");
 
-  const incrementCount = () => {
-    setCount(count + 1);
-  };
+  useEffect(() => {
+    const changeColorOnClick = () => {
+      if (color === "black") {
+        setColor("red");
+      } else {
+        setColor("black");
+      }
+    };
+    document.addEventListener("click", changeColorOnClick);
+
+    return () => {
+      document.removeEventListener("click", changeColorOnClick);
+    };
+  }, [color]);
 
   return (
     <div>
-      <div>{count}</div>
-      <button type="button" onClick={incrementCount}>
-        ++
-      </button>
+      <div
+        id="myDiv"
+        style={{
+          color: "white",
+          width: "100px",
+          height: "100px",
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          backgroundColor: color,
+        }}
+      >
+        {" "}
+        this div can change color. click me
+      </div>
     </div>
   );
 };
