52d078ebace058b851f1aa6e220b07624a5ea3bc
Klay Clarke <klayaclarke@gmail.com>
2022-02-23 16:20:51 -0500

    add links to each route

--- front-end/learning-js/react-router-demo/src/App.js
+++ front-end/learning-js/react-router-demo/src/App.js
@@ -1,6 +1,9 @@
+import { Link } from "react-router-dom";
+
 export const App = () => {
   return (
     <div>
+      <Link to={"/profile"}>Profile</Link>
       <h1>Hello from App</h1>
     </div>
   );
--- front-end/learning-js/react-router-demo/src/Profile.js
+++ front-end/learning-js/react-router-demo/src/Profile.js
@@ -1,6 +1,9 @@
+import { Link } from "react-router-dom";
+
 export const Profile = () => {
   return (
     <div>
+      <Link to={"/"}>App</Link>
       <h1>Hello from Profile</h1>
     </div>
   );
