61a0ca5b8ebe2d9ddf6c99b5c365d92ca8cabfc4
Klay Clarke <klayaclarke@gmail.com>
2022-03-11 12:25:15 -0500

    node js simple backend demo

---
+++ back-end/NODEJS--basic-informational-site/index.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "/about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "/contact-me.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/404.html"));
+});
+
+app.listen(3000);
