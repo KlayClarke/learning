d4f7d4661f9e75a90103254eb2147a02803927f3
Klay Clarke <klayaclarke@gmail.com>
2022-03-12 16:04:05 -0500

    routes etc

--- back-end/express-locallibrary-demo/routes/users.js
+++ back-end/express-locallibrary-demo/routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("Hello user");
 });
+
+router.get("/cool", function (req, res, next) {
+  res.send("You're so cool!");
+});
+
+// router.get("/");
 
 module.exports = router;
