6bb71f563226d3fe2ef2edeadd5f96ba1a5d7e9d
Klay Clarke <klayaclarke@gmail.com>
2022-03-14 16:56:14 -0400

    add code for bookinstance detail page

--- back-end/local-lib-express/controllers/bookinstanceController.js
+++ back-end/local-lib-express/controllers/bookinstanceController.js
@@ -18,8 +18,23 @@ exports.bookinstance_list = function (req, res, next) {
 
 // display detail page for a specific bookinstance
 
-exports.bookinstance_detail = function (req, res) {
-  res.send("NOT IMPLEMENTED: BookInstance detail: " + req.params.id);
+exports.bookinstance_detail = function (req, res, next) {
+  BookInstance.findById(req.params.id)
+    .populate("book")
+    .exec(function (err, bookinstance) {
+      if (err) {
+        return next(err);
+      }
+      if (bookinstance == null) {
+        let err = new Error();
+        err.status = 404;
+        return next(err);
+      }
+      res.render("bookinstance_detail", {
+        title: `Copy: ${bookinstance.book.title}`,
+        bookinstance,
+      });
+    });
 };
 
 // display bookinstance create form on GET
