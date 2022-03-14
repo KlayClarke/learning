d6ec580eb8a07ca2114038fb8d1d134d7dc70d51
Klay Clarke <klayaclarke@gmail.com>
2022-03-14 16:21:55 -0400

    add code for author details page

--- back-end/local-lib-express/controllers/authorController.js
+++ back-end/local-lib-express/controllers/authorController.js
@@ -1,4 +1,7 @@
 let Author = require("../models/author");
+let Book = require("../models/book");
+
+let async = require("async");
 
 // display list of all authors
 
@@ -18,8 +21,32 @@ exports.author_list = function (req, res, next) {
 
 // display detail page for a specific author
 
-exports.author_detail = function (req, res) {
-  res.send("NOT IMPLEMENTED: Author Detail: " + req.params.id);
+exports.author_detail = function (req, res, next) {
+  async.parallel(
+    {
+      author: function (callback) {
+        Author.findById(req.params.id).exec(callback);
+      },
+      authors_books: function (callback) {
+        Book.find({ author: req.params.id }, "title summary").exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.author == null) {
+        let err = new Error("Author not found");
+        err.status = 404;
+        return next(err);
+      }
+      res.render("author_detail", {
+        title: "Author Detail",
+        author: results.author,
+        author_books: results.authors_books,
+      });
+    }
+  );
 };
 
 // display author create form
