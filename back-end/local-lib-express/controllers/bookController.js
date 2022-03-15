453435916acc10378eef4795276ab543a0d49d97
Klay Clarke <klayaclarke@gmail.com>
2022-03-15 13:58:14 -0400

    add code for book creation form get and post handling

--- back-end/local-lib-express/controllers/bookController.js
+++ back-end/local-lib-express/controllers/bookController.js
@@ -2,8 +2,8 @@ let Book = require("../models/book");
 let Author = require("../models/author");
 let Genre = require("../models/genre");
 let BookInstance = require("../models/bookinstance");
-
 let async = require("async");
+const { body, validationResult } = require("express-validator");
 
 // display site index
 
@@ -85,15 +85,116 @@ exports.book_detail = function (req, res, next) {
 
 // display book create form on GET
 
-exports.book_create_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: Book create GET");
+exports.book_create_get = function (req, res, next) {
+  async.parallel(
+    {
+      authors: function (callback) {
+        Author.find(callback);
+      },
+      genres: function (callback) {
+        Genre.find(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      res.render("book_form", {
+        title: "Create Book",
+        authors: results.authors,
+        genres: results.genres,
+      });
+    }
+  );
 };
 
 // handle book create on POST
 
-exports.book_create_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Book create POST");
-};
+exports.book_create_post = [
+  // convert genre to an array
+  (req, res, next) => {
+    if (!req.body.genre instanceof Array) {
+      if (typeof req.body.genre === "undefined") req.body.genre = [];
+      else req.body.genre = new Array(req.body.genre);
+    }
+    next();
+  },
+
+  // validate and sanitize fields
+  body("title", "Title must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("author", "Author must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("summary", "Summary must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("isbn", "ISBN must not be empty").trim().isLength({ min: 1 }).escape(),
+  // use wildcard * to individually validate each of the genre array entries - code below translates to sanitize each item below key genre
+  body("genre.*").escape(),
+
+  // process request after validation and sanitization
+  (req, res, next) => {
+    // extract the validation errors from a request
+    const errors = validationResult(req);
+
+    // create a book object with escaped and trimmed data
+    let book = new Book({
+      title: req.body.title,
+      author: req.body.author,
+      summary: req.body.summary,
+      isbn: req.body.isbn,
+      genre: req.body.genre,
+    });
+
+    if (!errors.isEmpty()) {
+      // there are errors - render form again with sanitized values and error messages
+
+      // get all authors and genres for form
+      async.parallel(
+        {
+          authors: function (callback) {
+            Author.find(callback);
+          },
+          genres: function (callback) {
+            Genre.find(callback);
+          },
+        },
+        function (err, results) {
+          if (err) {
+            return next(err);
+          }
+          // mark user selected genres as checked
+          for (let i = 0; i < results.genres.length; i++) {
+            if (book.genre.indexOf(results.genres[i]._id) > -1) {
+              // set checked to true for current genre
+              results.genres[i].checked = "true";
+            }
+          }
+          res.render("book_form", {
+            title: "Create Book",
+            authors: results.authors,
+            genres: results.genres,
+            book: book,
+            errors: errors.array(),
+          });
+          return;
+        }
+      );
+    } else {
+      // data from form is valid
+
+      book.save(function (err) {
+        if (err) {
+          return next(err);
+        }
+        // successful - redirect to new book record
+        res.redirect(book.url);
+      });
+    }
+  },
+];
 
 // display book delete form on GET
 
