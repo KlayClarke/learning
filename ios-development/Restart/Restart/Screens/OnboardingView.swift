6d615b2b26fb5967ea3a596d2e4ce62d85fca708
Klay Clarke <clarkeklay@gmail.com>
2022-05-04 07:28:40 -0400

    add various animations to components / sections

--- ios-development/Restart/Restart/Screens/OnboardingView.swift
+++ ios-development/Restart/Restart/Screens/OnboardingView.swift
@@ -11,7 +11,8 @@ struct OnboardingView: View {
 	// MARK: - PROPERTY
 	@AppStorage("onboarding") var isOnboardingViewActive: Bool = true
 	@State private var buttonWidth: Double = UIScreen.main.bounds.width - 80 // make button width actual screen width minus 80 (pixels ??)
-	@State private var buttonOffset: CGFloat = 0
+	@State private var buttonOffset: CGFloat = 0 // initialize button offset as 0 -- change that whilst in app
+	@State private var isAnimating: Bool = false // initialize isAnimating variable as false -- I can change it to true under certain cases
 	// MARK: - BODY
     var body: some View {
 		ZStack {
@@ -20,13 +21,11 @@ struct OnboardingView: View {
 			VStack(spacing: 20) { // vstack allows its children to be arranged in a vertical line
 				// MARK: - HEADER
 				Spacer()
-				
 				VStack(spacing: 0) {
 					Text("Share.")
 						.font(.system(size: 60))
 						.fontWeight(.heavy)
 						.foregroundColor(.white)
-					
 					Text("""
 					It is not how much we give but
 					how much love we put into giving
@@ -37,13 +36,21 @@ struct OnboardingView: View {
 						.multilineTextAlignment(.center)
 						.padding(.horizontal, 10)
 				} //: HEADER
-					
+				.opacity(isAnimating ? 1 : 0)
+				.offset(y: isAnimating ? 0 : -40)
+				.animation(.easeOut(duration: 1), value: isAnimating)
+				// value is required - must tell swift what value / variable is causing the change and animation
+				// use shorthand conditionals to specify values of opacity and offset for animation
+				// header fade (opacity) in from top to bottom (offset y)
 				// MARK: - CENTER
 				ZStack { // zstack allows its children to be arranged atop each other
 					CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2) // use component
 					Image("character-1")
 						.resizable()
 						.scaledToFit()
+						.opacity(isAnimating ? 1 : 0)
+						.animation(.easeOut(duration: 0.5), value: isAnimating)
+					// image fade in (opacity) animation that takes 2 seconds to complete
 				} //: CENTER
 				Spacer()
 				// MARK: - FOOTER
@@ -90,11 +97,14 @@ struct OnboardingView: View {
 									}
 								}
 								.onEnded { _ in
-									if buttonOffset > buttonWidth/2 { // if user drags circle more than half the width of the container under button, snap circle to right of container and activate home view
-										buttonOffset = buttonWidth - 80
-										isOnboardingViewActive = false
-									} else {
-										buttonOffset = 0
+									withAnimation(Animation.easeOut(duration: 0.2)) {
+										// run animation when drag ends
+										if buttonOffset > buttonWidth/2 { // if user drags circle more than half the width of the container under button, snap circle to right of container and activate home view
+											buttonOffset = buttonWidth - 80
+											isOnboardingViewActive = false
+										} else {
+											buttonOffset = 0
+										}
 									}
 								}
 						) //: GESTURE
@@ -103,11 +113,18 @@ struct OnboardingView: View {
 				} //: FOOTER
 				.frame(width: buttonWidth, height: 80, alignment: .center)
 				.padding()
+				.opacity(isAnimating ? 1 : 0)
+				.offset(y: isAnimating ? 0 : 40)
+				.animation(.easeOut(duration: 1), value: isAnimating)
+				// footer drag bar fades in from bottom (1 second animation)
 			} //: VSTACK
 		} //: ZSTACK
+		.onAppear(perform: {
+			isAnimating = true // switch animation variable value from false to true when enter content initially renders
+		})
 	}
 }
-
+// MARK: - PREVIEW
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
         OnboardingView()
