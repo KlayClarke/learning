4344abf69fc3a9ba11c7c67e266336475fd15e14
Klay Clarke <clarkeklay@gmail.com>
2022-05-10 09:21:05 -0400

    drag gesture on image -- use function to reset image state on drag end

--- ios-development/Pinch/Pinch/Screen/ContentView.swift
+++ ios-development/Pinch/Pinch/Screen/ContentView.swift
@@ -11,9 +11,15 @@ struct ContentView: View {
 	// MARK: - PROPERTY
 	@State private var isAnimating: Bool = false
 	@State private var imageScale: CGFloat = 1
+	@State private var imageOffset: CGSize = .zero // can use .zero if width and height are zero
 	
 	// MARK: - FUNCTION
-	
+	func resetImageState() {
+		return withAnimation(.spring()) {
+			imageScale = 1
+			imageOffset = .zero
+		}
+	}
 	// MARK: - CONTENT
 	
     var body: some View {
@@ -28,6 +34,7 @@ struct ContentView: View {
 						.padding() // padding
 						.shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2) // add drop shadow to said image
 						.opacity(isAnimating ? 1 : 0)
+						.offset(x: imageOffset.width, y: imageOffset.height)
 						.animation(.linear(duration: 1), value: isAnimating)
 						.scaleEffect(imageScale)
 					// MARK: - 1. TAP GESTURE
@@ -38,10 +45,24 @@ struct ContentView: View {
 								}
 							} else {
 								withAnimation(.spring()) {
-									imageScale = 1
+									resetImageState()
 								}
 							}
 						}) // scale in or out on 2 (see "count:" above) taps
+					// MARK: - 2. DRAG GESTURE
+						.gesture(
+						DragGesture()
+							.onChanged { value in
+								withAnimation(.linear(duration: 1)) {
+									imageOffset = value.translation
+								}
+							} // drag gesture that offsets image by user drag motion offset
+							.onEnded { _ in // the underscore tells swift that we do not care about the animation value and to ignore it -- see .onChanged for the opposite
+								if imageScale <= 1 {
+									resetImageState()
+								}
+							}
+						)
 				} //: ZSTACK
 				.navigationTitle("Pinch & Zoom")
 				.navigationBarTitleDisplayMode(.inline)
